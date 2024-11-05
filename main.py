5c088785d3b3ae0dd4a93ed2ae15321f387b7dd5
DrewSmith1730 <drewsmith1730@gmail.com>
2024-11-05 17:10:08 -0600

    adjusted to an object oriented solution for window creation and such

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 # Rune Scape Data Processing Project
 
-# 
-#
+# At this point for xp loggint im going to have to create my own plugin to store and save that data
+# Logger for Combat and Grand exchange already added to my 
 #
 #
 #
 
 
 import sys
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget,QVBoxLayout
+from PyQt5.QtGui import QIcon
+
 import numpy
 import matplotlib
+import account
 
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    windows = QtWidgets.QWidget()
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'OSRS Data Processing Project'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
+        self.initUI()
 
-    windows.resize(500,500)
-    windows.move(100,100)
-    windows.show()
-    sys.exit(app.exec_())
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
