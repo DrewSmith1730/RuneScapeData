547fc09925d835f197e0c6751eb13c3af7275747
DrewSmith1730 <drewsmith1730@gmail.com>
2024-11-05 16:07:32 -0600

    add window creation, image folder, log folder, and basic size and icon image and gitignore file

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 # Rune Scape Data Processing Project
-# 
 
-## Focus on each account being its own object 
+# 
+#
+#
+#
+#
+
+
+import sys
+from PyQt5 import QtWidgets
+import numpy
+import matplotlib
+
 
 def main():
-    print("Hello World!")
+    app = QtWidgets.QApplication(sys.argv)
+    windows = QtWidgets.QWidget()
 
+    windows.resize(500,500)
+    windows.move(100,100)
+    windows.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
