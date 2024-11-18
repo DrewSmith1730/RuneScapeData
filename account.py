063522e39265addcf7db2a0741282be84dc3e3dc
DrewSmith1730 <drewsmith1730@gmail.com>
2024-11-18 13:51:29 -0600

    built out account class and added getters and setters along with comments for future plans for the project

--- account.py
+++ account.py
@@ -1,9 +1,62 @@
 # basic class to store basic important data pertaining to each individual account I have
 
 
-# basic concept 
-## added variables would be ( Total Skill level, )
+# Accout Class
+# Used to differentiate my different RuneScape Accounts
+
+# User -> String: Account Username
+# Type -> String: Account Type ie: (Main, IronMan, HCIronMan, etc) [Maybe swap to integer using a dictionary idk yet]
+# skilltotal -> Int: skill total : defualt 0
+# xptotal -> Int: Xp total : default 0
+# skills -> Int Array : for all skill levels
+# xp -> Int Array : xp totals for each individual skill
+# 
+
 
 class Account:
-    def __init__(self, user):
+    def __init__(self, user, type, skilltotal, xptotal, skills, xp):
         self.user = user
+        self.type = "Main"
+        self.skilltotal = skilltotal
+        self.xptotal = 0
+        self.skills = []
+        self.xp = 0
+
+    def get_user(self):
+        return self.user
+
+    def set_user(self, value):
+        self.user = value
+
+    def get_type(self):
+        return self.type
+
+    def set_type(self, value):
+        self.type = value
+
+    def get_skilltotal(self):
+        return self.skilltotal
+
+    def set_skilltotal(self, value):
+        self.skilltotal = value
+
+    def get_xptotal(self):
+        return self.xptotal
+
+    def set_xptotal(self, value):
+        self.xptotal = value
+
+    def get_skills(self):
+        return self.skills
+
+    def set_skills(self, value):
+        self.skills = value
+
+    def get_xp(self):
+        return self.xp
+
+    def set_xp(self, value):
+        self.xp = value
+
+
+
--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 # At this point for xp loggint im going to have to create my own plugin to store and save that data
 # Logger for Combat and Grand exchange already added to my 
 #
+# Us GE logger to log data for kill trips along with loot tracker to keep accurate info on kill numbers and money per hour minus cost of supplies
 #
-#
+# Use a jason file to save all data from prior runs of program so i dont have to reinit all my accounts over and over again
 
 
 import sys
@@ -14,6 +15,8 @@ from PyQt5.QtGui import QIcon
 import numpy
 import matplotlib
 import account
+
+
 
 
 class App(QWidget):
@@ -26,10 +29,12 @@ class App(QWidget):
         self.height = 480
         self.initUI()
 
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
